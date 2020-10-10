7ae8da8176bed58e62287f610d1fb4b34b3dfcca
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2020-10-10 12:19:52 +0200

    Add dark theme and copy button

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Username_Generator.Properties {
         /// <summary>
         ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap copy {
+            get {
+                object obj = ResourceManager.GetObject("copy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap username {
             get {
                 object obj = ResourceManager.GetObject("username", resourceCulture);
